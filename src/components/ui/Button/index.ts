96cfc96eebd40a035e109b2ff9f5e2c56d027848
Carafizi <gabriel@carafizi.com>
2024-12-11 16:02:08 -0300

    Initialize chore/replace-link-button

---
+++ src/components/ui/Button/index.ts
@@ -0,0 +1,3 @@
+export { default as ButtonLink } from './ButtonLink'
+export { default as ButtonSignIn } from './ButtonLink/ButtonSignIn'
+export { default as ButtonSignInFallback } from './ButtonLink/ButtonSignIn/ButtonSignInFallback'
